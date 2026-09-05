78a4f12cdb2ee930923c930ba7da7b568fefd53b
davidchen <ddwchensf@gmail.com>
2026-09-05 13:03:24 -0700

    add P206

--- src/main/kotlin/leetcode/P206.kt
+++ src/main/kotlin/leetcode/P206.kt
@@ -1,5 +1,9 @@
 package leetcode
 
+data class ListNode(val `val`: Int) {
+    var next: ListNode? = null
+}
+
 /**
  * Challenge: Reverse Linked List (easy)
  * Link: https://leetcode.com/problems/reverse-linked-list
@@ -9,20 +13,37 @@ object P206 {
 //IMPORTANT!! Submit Code Region Begin(Do not remove this line)
 
 //IMPORTANT!! Submit Code Region Begin(Do not remove this line)
-/**
- * Example:
- * var li = ListNode(5)
- * var v = li.`val`
- * Definition for singly-linked list.
- * class ListNode(var `val`: Int) {
- *     var next: ListNode? = null
- * }
- */
-class Solution {
-    fun reverseList(head: ListNode?): ListNode? {
-        
+    /**
+     * Example:
+     * var li = ListNode(5)
+     * var v = li.`val`
+     * Definition for singly-linked list.
+     * class ListNode(var `val`: Int) {
+     *     var next: ListNode? = null
+     * }
+     */
+    class Solution {
+        fun reverseList(head: ListNode?): ListNode? {
+            val (newHead, _) = reverseListHeadAndTail(head)
+            return newHead
+        }
+
+        fun reverseListHeadAndTail(head: ListNode?): Pair<ListNode?, ListNode?> =
+            when {
+                head == null -> Pair(null, null)
+                head.next == null -> {
+                    Pair(head, head)
+                }
+
+                else -> {
+                    val (newHead, newTail) = reverseListHeadAndTail(head.next)
+                    head.next = null
+                    newTail?.next = head
+                    Pair(newHead, head)
+                }
+            }
     }
-}
+
 //IMPORTANT!! Submit Code Region End(Do not remove this line)
 //IMPORTANT!! Submit Code Region End(Do not remove this line)
 

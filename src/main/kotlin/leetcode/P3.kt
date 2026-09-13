60d211a3391d7b9896449e84a7f22c5402bc1d5d
davidchen <ddwchensf@gmail.com>
2026-09-13 08:20:38 -0700

    update P3

--- src/main/kotlin/leetcode/P3.kt
+++ src/main/kotlin/leetcode/P3.kt
@@ -11,8 +11,28 @@ object P3 {
     //IMPORTANT!! Submit Code Region Begin(Do not remove this line)
     class Solution {
         fun lengthOfLongestSubstring(s: String): Int {
-            return 1
+            val inWindow = HashSet<Char>()
+            var mx = 0
+            var i = 0
+            var j = 0
+
+            while (j < s.length) {
+                if (!inWindow.contains(s[j])) {
+                    // If the character is unique, add it to the window
+                    inWindow.add(s[j])
+                    // Update the maximum length found so far
+                    mx = maxOf(mx, j - i + 1)
+                    j++
+                } else {
+                    // If it's a duplicate, shrink the window from the left
+                    inWindow.remove(s[i])
+                    i++
+                }
+            }
+
+            return mx
         }
+
     }
 //IMPORTANT!! Submit Code Region End(Do not remove this line)
 //IMPORTANT!! Submit Code Region End(Do not remove this line)

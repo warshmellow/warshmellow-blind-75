bbd8e42aa50916089871b1b559bb58138ab7bacf
davidchen <ddwchensf@gmail.com>
2026-09-13 16:27:32 -0700

    update P76

---
+++ src/main/kotlin/leetcode/P76.kt
@@ -0,0 +1,27 @@
+package leetcode
+
+/**
+ * Challenge: Minimum Window Substring (hard)
+ * Link: https://leetcode.com/problems/minimum-window-substring
+ */
+object P76 {
+
+//IMPORTANT!! Submit Code Region Begin(Do not remove this line)
+
+    //IMPORTANT!! Submit Code Region Begin(Do not remove this line)
+    class Solution {
+        fun minWindow(s: String, t: String): String {
+            return ""
+        }
+    }
+//IMPORTANT!! Submit Code Region End(Do not remove this line)
+//IMPORTANT!! Submit Code Region End(Do not remove this line)
+
+    // Local Testing Area (Ignored by LeetCode during submission)
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val solution = Solution()
+        // Try your test cases here!
+        // println(solution.findMedianSortedArrays(intArrayOf(1, 3), intArrayOf(2)))
+    }
+}

5591d5ddb74cdd7bd1214b3e0b770237f9ed46b8
davidchen <ddwchensf@gmail.com>
2026-09-13 12:17:38 -0700

    update P11

---
+++ src/main/kotlin/leetcode/P11.kt
@@ -0,0 +1,27 @@
+package leetcode
+
+/**
+ * Challenge: Container With Most Water (medium)
+ * Link: https://leetcode.com/problems/container-with-most-water
+ */
+object P11 {
+
+//IMPORTANT!! Submit Code Region Begin(Do not remove this line)
+
+    //IMPORTANT!! Submit Code Region Begin(Do not remove this line)
+    class Solution {
+        fun maxArea(height: IntArray): Int {
+            return 1
+        }
+    }
+//IMPORTANT!! Submit Code Region End(Do not remove this line)
+//IMPORTANT!! Submit Code Region End(Do not remove this line)
+
+    // Local Testing Area (Ignored by LeetCode during submission)
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val solution = Solution()
+        // Try your test cases here!
+        // println(solution.findMedianSortedArrays(intArrayOf(1, 3), intArrayOf(2)))
+    }
+}

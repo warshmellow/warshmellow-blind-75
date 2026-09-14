c9287d7ffd83c6def3e291a0f29358d9cad75202
davidchen <ddwchensf@gmail.com>
2026-09-13 17:03:34 -0700

    update

--- src/main/kotlin/leetcode/P76.kt
+++ src/main/kotlin/leetcode/P76.kt
@@ -11,7 +11,56 @@ object P76 {
     //IMPORTANT!! Submit Code Region Begin(Do not remove this line)
     class Solution {
         fun minWindow(s: String, t: String): String {
-            return ""
+            if (s.length < t.length) return ""
+
+            val asciiSize = 128
+            val countT = IntArray(asciiSize)
+            val window = IntArray(asciiSize)
+
+            var uniqueCharsInT = 0
+            for (i in 0 until t.length) {
+                val charCode = t[i].code
+                if (countT[charCode] == 0) uniqueCharsInT++
+                countT[charCode]++
+            }
+
+            var have = 0
+            val need = uniqueCharsInT
+
+            var minLen = Int.MAX_VALUE
+            var resRange = -1..-1
+
+            var left = 0
+            for (right in 0 until s.length) {
+                val rChar = s[right].code
+                window[rChar]++
+
+                val existsInCountT = countT[rChar] > 0
+                val windowHasSameCountChar = window[rChar] == countT[rChar]
+                if (existsInCountT && windowHasSameCountChar) have++
+
+                while (have == need) {
+                    val currentWindowLen = right + 1 - left
+                    if (currentWindowLen < minLen) {
+                        minLen = currentWindowLen
+                        resRange = left..right
+                    }
+
+                    val lChar = s[left].code
+                    window[lChar]--
+
+                    if (countT[lChar] > 0 && window[lChar] < countT[lChar]) {
+                        have--
+                    }
+                    left++
+                }
+            }
+
+            return if (minLen == Int.MAX_VALUE) {
+                ""
+            } else {
+                s.substring(resRange)
+            }
         }
     }
 //IMPORTANT!! Submit Code Region End(Do not remove this line)

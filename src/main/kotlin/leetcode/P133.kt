9057ae7fee64231cd8c9b939f2fa2d7c518eb886
davidchen <ddwchensf@gmail.com>
2026-09-14 22:39:47 -0700

    update P133

---
+++ src/main/kotlin/leetcode/P133.kt
@@ -0,0 +1,34 @@
+package leetcode
+
+/**
+ * Challenge: Clone Graph (medium)
+ * Link: https://leetcode.com/problems/clone-graph
+ */
+object P133 {
+
+//IMPORTANT!! Submit Code Region Begin(Do not remove this line)
+
+//IMPORTANT!! Submit Code Region Begin(Do not remove this line)
+    /**
+     * Definition for a Node.
+     * class Node(var `val`: Int) {
+     *     var neighbors: ArrayList<Node?> = ArrayList<Node?>()
+     * }
+     */
+
+    class Solution {
+        fun cloneGraph(node: Node?): Node? {
+            return node
+        }
+    }
+//IMPORTANT!! Submit Code Region End(Do not remove this line)
+//IMPORTANT!! Submit Code Region End(Do not remove this line)
+
+    // Local Testing Area (Ignored by LeetCode during submission)
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val solution = Solution()
+        // Try your test cases here!
+        // println(solution.findMedianSortedArrays(intArrayOf(1, 3), intArrayOf(2)))
+    }
+}

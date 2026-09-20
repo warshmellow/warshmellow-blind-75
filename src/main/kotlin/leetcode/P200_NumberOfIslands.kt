13a5d67000de9e87b91c3712f28cf4830ae50f12
davidchen <ddwchensf@gmail.com>
2026-09-19 21:23:07 -0700

    add p200

--- src/main/kotlin/leetcode/P200_NumberOfIslands.kt
+++ src/main/kotlin/leetcode/P200_NumberOfIslands.kt
@@ -11,7 +11,27 @@ object P200_NumberOfIslands {
     //IMPORTANT!! Submit Code Region Begin(Do not remove this line)
     class Solution {
         fun numIslands(grid: Array<CharArray>): Int {
-            return 0
+            var total = 0
+            for (i in grid.indices) {
+                for (j in grid[0].indices) {
+                    if (grid[i][j] == '1') {
+                        dfs(grid, i, j)
+                        total++
+                    }
+                }
+            }
+            return total
+        }
+
+        private fun dfs(grid: Array<CharArray>, i: Int, j: Int) {
+            if (i !in grid.indices || j !in grid[0].indices || grid[i][j] == '0') return
+
+            grid[i][j] = '0'
+
+            dfs(grid, i + 1, j)
+            dfs(grid, i - 1, j)
+            dfs(grid, i, j + 1)
+            dfs(grid, i, j - 1)
         }
     }
 //IMPORTANT!! Submit Code Region End(Do not remove this line)
